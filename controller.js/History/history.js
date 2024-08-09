27d9640a0aba4f1fbe71ed864341fc1137bea832
vishal.kale <vishal.kale@artoon.in>
2024-08-10 01:02:36 +0530

    Get intraday candle data with a 30-minute interval
    Get intraday candle data with a 30-minute interval

--- controller.js/History/history.js
+++ controller.js/History/history.js
@@ -26,6 +26,30 @@ apiInstance.getHistoricalCandleData1(instrumentKey, interval, to_date, from_date
 }
 }
 
+const getIntraDayCandleData = async(req , res) =>{
+    try {
+
+let apiInstance = new UpstoxClient.HistoryApi();
+
+ apiVersion = "2.0"; 
+const {instrumentKey  , interval} = req.params  
+
+apiInstance.getIntraDayCandleData(instrumentKey, interval, apiVersion, (error, data, response) => {
+    if (error) {
+      console.error(error);
+      return sendError(res ,"getIntraDayCandleData" , JSON.parse(response.text) )
+    } else {
+        console.log('API called successfully. Returned data: ' + JSON.stringify(data));
+        sendSuccess(res , JSON.parse(response.text) )
+    }
+});
+} catch (error) {
+        console.error(error);
+        sendError(res ,"getIntraDayCandleData" , error.message) 
+
+    }
+}
+
 module.exports = {
-    HistoryCandleData
+    HistoryCandleData , getIntraDayCandleData
 }
--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const { getLoginUrl, handleCallback } = require('../controller.js/Login/Auth/LoginAuth');
 const { getUserProfile, getUserProfileFundAndMargine } = require('../controller.js/user/user');
 const getBrokerage = require('../controller.js/Charge/charges');
-const { HistoryCandleData } = require('../controller.js/History/history');
+const { HistoryCandleData, getIntraDayCandleData } = require('../controller.js/History/history');
 
 
 router.get('/login', getLoginUrl);
@@ -13,6 +13,7 @@ router.get('/user/profile', getUserProfile);
 router.get('/user/get-funds-and-margin', getUserProfileFundAndMargine);
 router.get('/charges/brokerage', getBrokerage);
 router.get('/historical-candle/:instrumentKey/:interval/:to_date/:from_date', HistoryCandleData);
+router.get('/historical-candle/intraday/:instrumentKey/:interval', getIntraDayCandleData);
 // router.get('/place', upstoxController.placeOrder);
 // router.get('/candle', upstoxController.getCandleData);
 // router.post("/getFullMarketQuote" , upstoxController.getFullMarketQuote);
